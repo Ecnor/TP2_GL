caaa11e50a1f115d804fc08f7520b281da8d22c9
Emine Bernardone <bernardoneemine@gmail.com>
2017-10-10 11:07:15 +0200

    implemented fizzbuzz, created and implemented fizzbuzztest, updated pom.xml

--- src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,20 @@
 package fr.unistra.fizzbuzz;
 
 public class FizzBuzz {
-
+	
+	public String convert(int number) {
+		String res = "";
+		
+		if(number % 3 == 0) {
+			res += "Fizz";
+		}
+		if(number % 5 == 0) {
+			res += "Buzz";
+		}
+		if(res.isEmpty()) {
+			res += number;
+		}
+		
+		return res;
+	}
 }
